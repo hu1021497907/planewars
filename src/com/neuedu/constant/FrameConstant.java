b8d5a70b9e7aa44ade9456b95e5d5f5322ee12fd
Your Name <you@example.com>
2019-07-30 15:38:42 +0800

    背景添加

---
+++ src/com/neuedu/constant/FrameConstant.java
@@ -0,0 +1,9 @@
+package com.neuedu.constant;
+
+public class FrameConstant {
+
+    public static final int FRAME_WIDTh = 800;
+    public static final int FRAME_HEIGHT = 1000;
+
+    public static final int GAME_SPEED = 1;
+}
---
+++ src/com/neuedu/runtime/Background.java
@@ -0,0 +1,34 @@
+package com.neuedu.runtime;
+
+import com.neuedu.base.BaseSprite;
+import com.neuedu.base.Drawable;
+import com.neuedu.base.Moveable;
+import com.neuedu.constant.FrameConstant;
+import com.neuedu.util.ImageMap;
+
+import java.awt.*;
+
+public class Background extends BaseSprite implements Moveable, Drawable {
+
+    private Image image;
+
+    public Background() {
+        this(0, FrameConstant.FRAME_HEIGHT -ImageMap.get("bg01").getHeight(null), ImageMap.get("bg01"));
+    }
+
+    public Background(int x, int y, Image image) {
+        super(x, y);
+        this.image = image;
+    }
+
+    @Override
+    public void move() {
+        setY(getY() + FrameConstant.GAME_SPEED);
+    }
+
+    @Override
+    public void draw(Graphics g) {
+        g.drawImage(image,getX(),getY(),image.getWidth(null),image.getHeight(null),null);
+        move();
+    }
+}
